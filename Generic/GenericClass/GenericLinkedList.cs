3958f4f88bc06390d74c22893787e5da040b3837
MishaSylenko <mishasilen@gmail.com>
2025-09-16 09:53:20 +0200

    Generic

---
+++ Generic/GenericClass/GenericLinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace Generic.GenericClass;
+
+public class GenericLinkedList<T>
+{
+    class Node
+    {
+        public T Data;
+        public Node Next;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    private Node head;
+    
+    public void Add(T data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,20 @@
+﻿using Generic.GenericClass;
+
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenericLinkedList<int> intList = new GenericLinkedList<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+
+            GenericLinkedList<string> stringList = new GenericLinkedList<string>();
+            stringList.Add("A");
+            stringList.Add("B");
+            stringList.Add("C");
+        }
+    }
+}
