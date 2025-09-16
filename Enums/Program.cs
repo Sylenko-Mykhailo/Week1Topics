0b5324f00241abc2cc54dafbd2d08d6d3fcd65fd
MishaSylenko <mishasilen@gmail.com>
2025-09-16 10:27:41 +0200

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Enums
+{
+    public enum DayOfWeek : byte
+    {
+        Sunday,
+        Monday,
+        Tuesday = 251,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek today = DayOfWeek.Wednesday;
+            Console.WriteLine($"Today is: {today}");
+
+            if (today == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("It's midweek!");
+            }
+            Console.WriteLine($"Numeric value of  is {(byte)System.DayOfWeek.Friday}");
+            
+            
+            foreach (var day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine($"{day} = {(byte)day}");
+            }
+        }
+    }
+}
+
