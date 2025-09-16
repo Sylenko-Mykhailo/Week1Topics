7fea2df563b411861615c27fe910420aabba3222
MishaSylenko <mishasilen@gmail.com>
2025-09-16 11:10:31 +0200

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12, b = 25, Result;
+            // Bitwise AND Operator (If both of the bits are 1, it gives 1.)
+            Result = a & b;
+            Console.WriteLine($"Bitwise AND: {Result}");
+            // Bitwise OR Operator (If any of the bits is 1, it gives 1.)
+            Result = a | b;
+            Console.WriteLine($"Bitwise OR: {Result}");
+            // Bitwise XOR Operator (If both of the bits are different, it gives 1.)
+            Result = a ^ b;
+            Console.WriteLine($"Bitwise XOR: {Result}");
+            
+            var boo = true;
+            
+            var res = boo ? "Boo" : "Not Boo";
+            Console.WriteLine(res);
+        }
+    }
+}
+
