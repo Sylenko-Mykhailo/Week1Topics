ad7f8f761cbcde2da2ec9eb7e71d96be021ba455
MishaSylenko <mishasilen@gmail.com>
2025-09-16 10:20:21 +0200

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"For Loop Iteration: {i}");
+            }
+
+            // While Loop
+            int j = 0;
+            while (j < 5)
+            {
+                Console.WriteLine($"While Loop Iteration: {j}");
+                j++;
+            }
+
+            // Do-While Loop
+            int k = 0;
+            do
+            {
+                Console.WriteLine($"Do-While Loop Iteration: {k}");
+                k++;
+            } while (k < 5);
+
+            // Foreach Loop
+            string[] fruits = { "Apple", "Banana", "Cherry" };
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine($"Fruit: {fruit}");
+            }
+        }
+    }
+}
