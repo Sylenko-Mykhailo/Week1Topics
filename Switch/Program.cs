f543c159b3c8d0412ff8d31d0fc2ea716cba95bb
MishaSylenko <mishasilen@gmail.com>
2025-09-16 11:16:55 +0200

    Switch

---
+++ Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "C#";
+            Console.WriteLine("Switch Statement Started");
+            switch (str)
+            {
+                case "C#":      
+                case "Java":
+                case "C":
+                    Console.WriteLine("It's a Programming Language");
+                    break;
+                case "MSSQL":
+                case "MySQL":
+                case "Oracle":
+                    Console.WriteLine("It's a Database");
+                    break;
+                case "MVC":
+                case "WEB API":
+                    Console.WriteLine("It's a Framework");
+                    break;
+            }
+            Console.WriteLine("Switch Statement Ended");
+            Console.ReadKey();
+        }
+    }
+}
